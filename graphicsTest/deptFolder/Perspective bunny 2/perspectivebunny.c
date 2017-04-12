498e0534155988831fc125231a61aa0473f1fd94
chrjo915 <chrjo915@student.liu.se>
2017-04-12 16:28:04 +0200

    keyboard input within the graphicTest folder and updated the readme

--- graphicsTest/deptFolder/Perspective bunny 2/perspectivebunny.c
+++ graphicsTest/deptFolder/Perspective bunny 2/perspectivebunny.c
@@ -18,6 +18,9 @@
 #include "loadobj.h"
 #include "LoadTGA.h"
 #include "VectorUtils3.h"
+#include <stdio.h>
+#include <stdarg.h> // va_*
+#include <string.h> // strlen, strcpy
 
 Model *m;
 // Reference to shader program
@@ -66,9 +69,9 @@ void display(void)
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
     // a += 0.1;
-    mat4 rotationMatrix = Mult(Rz(a / 3), Ry(a));
-    glUniformMatrix4fv(glGetUniformLocation(program, "mdlMatrix"), 1, GL_TRUE, rotationMatrix.m);
-    DrawModel(m, program, "inPosition", "inNormal", 0L);
+    // mat4 rotationMatrix = Mult(Rz(a / 3), Ry(a));
+    // glUniformMatrix4fv(glGetUniformLocation(program, "mdlMatrix"), 1, GL_TRUE, rotationMatrix.m);
+    // DrawModel(m, program, "inPosition", "inNormal", 0L);
 
     printError("display");
 
@@ -92,36 +95,83 @@ void sendToARC(struct testStruct command)
 
 struct testStruct changeSpeed(int deltaSpeed)
 {
-    printf((char)deltaSpeed);
-    struct testStruct t={deltaSpeed};
-    return t;
+
+    // printf((char)deltaSpeed);
+    // struct testStruct t={deltaSpeed};
+    // return t;
 }
 
+char *concat(int count, ...)
+{
+    va_list ap;
+    int i;
+
+    // Find required length to store merged string
+    int len = 1; // room for NULL
+    va_start(ap, count);
+    for (i = 0; i < count; i++)
+        len += strlen(va_arg(ap, char *));
+    va_end(ap);
+
+    // Allocate memory to concat strings
+    char *merged = calloc(sizeof(char), len);
+    int null_pos = 0;
+
+    // Actually concatenate strings
+    va_start(ap, count);
+    for (i = 0; i < count; i++)
+    {
+        char *s = va_arg(ap, char *);
+        strcpy(merged + null_pos, s);
+        null_pos += strlen(s);
+    }
+    va_end(ap);
+
+    return merged;
+}
+
+void sendControlCommand(int speed, int theta)
+{
+    int checksum = 0x20 + 0 + 0 + speed + theta;
+
+    char *temp;
+    temp = concat(8, "echo -e \"", (char)0x20, (char)0, (char)0, (char)checksum, (char)speed, (char)theta, "\" > fifo_to_control");
+    printf("echo -e \" %x %x %x %x %x %x \" > fifo_to_control", 0x20, 0, 0, checksum, speed, theta);
+    printf(temp);
+    free(temp);
+}
 void key(unsigned char key, int x, int y)
 {
     switch (key)
     {
+    case 't':
+        // the following need to be fixed.
+        // sendControlCommand('A', 'B');
+        break;
+    case 'c':
+        system("ssh -t -p 4444 arc@nhikim91.ddns.net ssh -p 2222 pi@localhost");
+        break;
     case 'w':
-	printError("testError%d\n");
-	sendToARC(changeSpeed(deltaSpeed));
-	break;
+        printf("echo -e \" %x %x %x %x %x %x \" > fifo_to_control", 0x20, 0, 0, 0xA4, 'A', 'B');
+            // sendToARC(changeSpeed(deltaSpeed));
+            break;
     case 's':
-	sendToARC(changeSpeed(-deltaSpeed));
-	break;
+        // sendToARC(changeSpeed(-deltaSpeed));
+        system("whoami");
+        break;
     case 'd':
-	// sendToARC(changeDirection(deltaTheta));
-	break;
+        // sendToARC(changeDirection(deltaTheta));
+        break;
     case 'a':
-	// sendToARC(changeDirection(-deltaTheta));
-	break;
+        // sendToARC(changeDirection(-deltaTheta));
+        break;
     default:
-    break;
+        break;
     }
 }
 
 int main(int argc, char *argv[])
 {
-    printf("testErrorafssweafsdfasgsdaga");
     glutInit(&argc, argv);
     glutInitContextVersion(3, 2);
     glutCreateWindow("GL3 obj and perspective example with VectorUtils");
--- readme.txt
+++ readme.txt
@@ -19,4 +19,14 @@ libgl1-mesa-dev
 libxmu-dev 
 libxi-dev
 
+Connect over ssh
+At the car input the command: ssh -N -R 2222:localhost:22 -p 4444 arc@nhkim91.ddns.net
 
+At the user input the command: ssh -t -p 4444 arc@nhikim91.ddns.net ssh -p 2222 pi@localhost
+
+User name (user): arc, password: stavarett
+User name (car): pi, password: stavarett
+
+To compile the graphicTest file compile through command while in the graphicTest/depthFolder/Perspective bunny 2.
+gcc perspectivebunny.c ../../common/*.c ../../common/Linux/*.c -lGL -o test -I../../common -I../../common/Linux -DGL_GLEXT_PROTOTYPES  -lXt -lX11 -lm
+This will create a exicutable named test that takes simple keyboard commands. In the perspectivebunny.c you can change said commands and play around.
