fcaa92adcc06380887761fba76121397a461b8d6
chrjo915 <chrjo915@student.liu.se>
2017-04-23 15:57:43 +0200

    shell script for pi to connect to server

--- pi.sh
+++ pi.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 #Make sure the pi is connected to the server. Make sure to give the file permission by chmod 755 pi.sh. Move the script into the bin folder so that it will be seen in path.
-autossh ssh -N -R 2222:localhost:22 -p 4444 arc@nhkim91.ddns.net
-stavarett
-stavarett
+autossh -N -R 2222:localhost:22 -p 4444 arc@nhkim91.ddns.net && sleep 1 && stavarett && sleep 1 && stavarett
