127c41e537c678e0a81299d7b02b689ac9445ce5
dahlemil <emida919@student.liu.se>
2017-05-02 08:49:09 +0200

    Översatte

--- sensor/Sensor_module.c
+++ sensor/Sensor_module.c
@@ -1,41 +1,24 @@
-/*
- * Sensor_module.c
- *
- * Created: 3/31/2017 8:48:54 AM
- *  Author: emida919
- */ 
-
 #include <asf.h>
 #include <avr/io.h>
 #include <avr/interrupt.h>
-#include <avr/sleep.h>
 #include <stdint.h>
 #include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
-#define 	F_CPU   8000000UL 
-#include <util/delay.h>
-
-//#include "spi_slave.h"
-
-#define DDR_SPI DDRB
-#define DD_MISO 6
 
 volatile uint16_t timer0_overflow = 0;
 volatile uint16_t timer1_overflow = 0;
-volatile uint16_t time_from0 = 0;
-volatile uint16_t time_from1 = 0;
 uint16_t time_sec0[] = {0, 0, 0};
 uint16_t time_sec1[] = {0, 0, 0};
-//uint8_t median_speed = 0;
-//uint8_t median_time0[] = {0, 0};
-//uint8_t median_time1[] = {0, 0};
 uint16_t median_time0 = 0;
 uint16_t median_time1 = 0;
-//const uint16_t min_diff = 1000;	
-//uint16_t sent_median_time0 = 0;
-//uint16_t sent_median_time1 = 0;
 
+unsigned char state = 'W';
+uint8_t data[4];
+int8_t dataindex;
+
+#define DDR_SPI DDRB
+#define DD_MISO 6
 
 uint16_t median(uint16_t time[]);
 void shift(uint16_t array[], int n);
@@ -43,98 +26,59 @@ void set_zero(uint16_t time[], int n);
 uint16_t average(uint16_t x, uint16_t y);
 int cmpfunc (const void * a, const void * b);
 void handleNewData(uint8_t* data, uint16_t length);
-void spiInit(void); // enable SPI
+void spiInit(void);
+void enable_clocks(void);
+void enable_sensors(void);
 
 
-ISR(TIMER0_OVF_vect)		//Tidsräkning för hjul 1
+ISR(TIMER2_OVF_vect)	//Counting the time until next interrupt
 {
 	timer0_overflow++;
-	
-	if(timer0_overflow >= 10000) //Om en mätning tar längre än tid ca 200ms så säger vi att bilen står stilla
+		
+	if(timer0_overflow >= 255)	 //If a measurement takes longer than ~260ms we define that as the car not moving
 	{
 		set_zero(time_sec0, 2);
+		median_time0 = 0;
 		timer0_overflow = 0;
-	}	
-}
-
-ISR(TIMER2_OVF_vect)		 //Tidsräkning för hjul 1
-{
-	timer1_overflow++;
+		
+	}
 	
-	if(timer1_overflow >= 10000)  //Om en mätning tar längre än tid ca 200ms så säger vi att bilen står stilla
+	timer1_overflow++;
+		
+	if(timer1_overflow >= 255)  //If a measurement takes longer than ~260ms we define that as the car not moving
 	{
 		set_zero(time_sec1, 2);
+		median_time1 = 0;
 		timer1_overflow = 0;
 	}
 }
 
-ISR(INT0_vect)			//Avbrott då magnetsensorn känner av en magnet
+
+ISR(INT0_vect)	//Interrupt when the hall sensor is responding to a magnetic field
 {
-	shift(time_sec0, 2);			//vi shiftar tidsmätningarna (arrayen) 
-	time_sec0[0] = timer0_overflow;	//lägger in nytt mätvärde i arrayen
+	shift(time_sec0, 2);	//shifting the measurements in the  array 
+	time_sec0[0] = timer0_overflow;
 	median_time0 = median(time_sec0);
-	/*
-	uint8_t tmp_median_time = median(time_sec0);
-	median_speed = median_speed && 0xf0;
-
-	if(tmp_median_time != 0)
-	{
-		median_speed += 2454 / tmp_median_time;
-	}
-	*/	/*
-	uint16_t tmp_median0 = median(time_sec0);
-	int diff = abs(tmp_median0 - sent_median_time0);	//nuvarande median - senaste median
-	
-	if (diff > min_diff)	//Om det nya mätvärdet skiljer sig mer än min_diff, så uppdaterar vi hastigheten
-	{
-		median_time0[0] = ((tmp_median0 >> 8) && 0xff) + 0x80;  //vi delar upp det nya medianvärdet, från
-		median_time0[1] = (tmp_median0 >> 0) && 0xff;			//ett 16-bitarsvärde till 8-bitarsarray
-		sent_median_time0 = tmp_median0;				
-		spiSend(SEND_DATA, DYNMAP, median_time0, 16);
-	}
-	*/
-
 	timer0_overflow = 0;	
 }
 
 
-ISR(INT1_vect)			//samma som ovan fast för andra hjulet
+ISR(INT1_vect)	//Same as above but for the other wheel
 {
 	shift(time_sec1, 2);
 	time_sec1[0] = timer1_overflow;
 	median_time1 = median(time_sec1);
-
-	/*
-	uint8_t tmp_median_time = median(time_sec0);
-	median_speed =  median_speed && 0x0f;
-
-	if(tmp_median_time != 0)
-	{
-		median_speed += (2454 / tmp_median_time) << 4;
-	}
-	*/
-	/*
-	uint16_t tmp_median1 = median(time_sec1);
-	int diff = abs(tmp_median1 - sent_median_time1);
-
-	if (diff > min_diff)
-	{
-	median_time1[0] = (tmp_median1 >> 8) && 0xff;
-	median_time1[1] = (tmp_median1 >> 0) && 0xff;
-	sent_median_time1 = tmp_median1;		
-	spiSend(SEND_DATA, DYNMAP, median_time1, 16);
-	}
-	*/
-
 	timer1_overflow = 0;
 }
 
-int cmpfunc (const void * a, const void * b)	//hjälpfunk för att sortera värden i storleksordning
+
+int cmpfunc (const void * a, const void * b)	//helpfunction to sort the measurements in increasing order
 {
 	return ( *(uint16_t*)a - *(uint16_t*)b );
 }
 
-uint16_t median(uint16_t time[])				//beräkning av median
+
+uint16_t median(uint16_t time[])	//beräkning av median
 {
 	uint16_t time_copy[] = {time[0], time[1], time[2]};
 	qsort(time_copy, 3, sizeof(uint16_t), cmpfunc);
@@ -142,7 +86,7 @@ uint16_t median(uint16_t time[])				//beräkning av median
 }
 
 
-void shift(uint16_t array[], int n)				//skiftar en array med n element EN gång
+void shift(uint16_t array[], int n)		//shifting an array with n elements ONE time
 {
 	for(int i = n; i > 0; i--)
 	{
@@ -150,7 +94,8 @@ void shift(uint16_t array[], int n)				//skiftar en array med n element EN gång
 	}
 }
 
-void set_zero(uint16_t time[], int n)			//nollställer en array med n element
+
+void set_zero(uint16_t time[], int n)	//resets an array with n elements
 {
 	for(int i = 0; i <= n; i ++)
 	{
@@ -158,40 +103,18 @@ void set_zero(uint16_t time[], int n)			//nollställer en array med n element
 	}
 }
 
-/*
-void handleNewData(uint8_t* data, uint16_t length)
-{
-	// Do something with the data received from the bus.
-}
-*/
-/*
-void spiInit(void) // enable SPI
+
+void spiInit(void) // enabling SPI
 {	
 	DDR_SPI = (1<<DD_MISO);
-	SPCR = (1<<SPE)|(0<<CPOL)|(1<<SPIE);
-}
-
-ISR (SPI_STC_vect)
-{
-	SPDR = median_speed;
-}
-*/
-void spiInit(void) // enable SPI
-{	
-	DDR_SPI = (1<<DD_MISO);
-	SPCR = (1<<SPE)|(0<<CPOL)|(0<<CPHA)|(1<<SPIE);
-	//DDRA |= 0xFF;
-	//PORTA = SPDR;
-	uint8_t tmp = SPDR;
+	SPCR = (1<<SPE)|(0<<CPOL)|(0<<CPHA)|(1<<SPIE)|(1<<SPR1);
+	uint8_t tmp =  SPDR;
 }
 
 
-//Interrupt för sensormodul, byt ut variabeln send mot de 4 bytes som ska skickas [fyll i med något om 4 bytes är för mycket]
+//Interrupt for sensor module, the variable send = the 4 bytes which we're gonna send [fyll i med något om 4 bytes är för mycket]
 ISR(SPI_STC_vect)
 {
-	static uint8_t data[4];
-	static int8_t dataindex;
-	static unsigned char state = 'W';
 	uint8_t inbyte;
 	//read Data Register
 	inbyte = SPDR;
@@ -200,55 +123,52 @@ ISR(SPI_STC_vect)
 		case 'W':
 			if (inbyte == 0xFF)
 			{
-                TIMSK0 = (0 << TOIE0);		//disable overflow_interrupts 
-                TIMSK2 = (0 << TOIE2);		//disable overflow_interrupts 
-                //int32_t send = (count/4);
-				int32_t send = (median_time1 << 16) + median_time0;
 				//convert to an array of bytes
-				SPDR = (uint8_t)(send & 0xff);
-				data[0] = (uint8_t)(send & 0xff);
-				data[1] = (uint8_t)((send >> 8) & 0xff);
-				data[2] = (uint8_t)((send >> 16) & 0xff);
-				data[3] = (uint8_t)((send >> 24) & 0xff);
+				SPDR = (uint8_t)(median_time0  & 0xff);
+				data[0] = (uint8_t)(median_time0  & 0xff);
+				data[1] = (uint8_t)((median_time0 >> 8) & 0xff);
+				data[2] = (uint8_t)((median_time1) & 0xff);
+				data[3] = (uint8_t)((median_time1 >> 8) & 0xff);
 				state = 'S';
 				dataindex = 1;
-				//SPDR = data[0]; //Can be removed(or not?)
 			}
 			break;
+			
 		case 'S':
 			SPDR = data[dataindex];
 			dataindex += 1;
 				if (dataindex == 4)
 				{
 					state = 'W';
-                    TIMSK0 = (1 << TOIE0);		//enable overflow_interrupts 
-                    TIMSK2 = (1 << TOIE2);		//enable overflow_interrupts 
 				}
 		break;
 	}
 }
 
+
+void enable_clocks(void)
+{
+	TCNT2 = 0;			//a special slower clock
+	TIMSK2 = (1 << TOIE2);		//enable overflow_interrupts
+	TCCR2B = (1 << CS21) | (1 << CS20);//	 (1 << CS22);// | (1 << CS20);		//startar timer_counter0
+}
+
+
+void enable_sensors(void)
+{
+	DDRD = 0x00;			//datadirection to input D-reg
+	EICRA = (1 << ISC11) | (1 << ISC10) | (1 << ISC01) | (1 << ISC00);	//interrupts at a rising edge
+	EIMSK = (1 << INT1) | (1 << INT0);	//extern interrupts happens at INT1 & INT0
+}
+
+
 int main(void)
 {
-
-	//Start clock
-	TCNT0 = 0;			//speciell klocka som går långsamt
-	TIMSK0 = (1 << TOIE0);		//enable overflow_interrupts 
-	TCCR0B = (1 << CS00);		//startar timer_counter0
-	
-	TCNT2 = 0;			//speciell klocka som går långsamt
-	TIMSK2 = (1 << TOIE2);		//enable overflow_interrupts 
-	TCCR2B =  (1 << CS20);		//startar timer_counter0
-	
-	DDRD = 0x00;			//datadirection till insignal D-reg
-	EICRA = (1 << ISC11) | (1 << ISC10) | (1 << ISC01) | (1 << ISC00);	//interrupts vid endast högflank
-	EIMSK = (1 << INT1) | (1 << INT0);	//externa interrupts sker på INT1 & INT0
+	enable_clocks();
+	enable_sensors();
 	spiInit();
-	//spiSlaveInit(DYNMAP, &handleNewData);
-	sei();				//enable interrupts
+	sei();				//enabling interrupts
 	
     while(1)
     {}
 }
-
-// 9,4 ms är minsta tiden mellan två mätningar
