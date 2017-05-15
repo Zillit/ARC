303c9fb1ffb2bbaa6d7de474baa64ccd8cca2afc
antpo364 <antpo364@student.liu.se>
2017-05-15 10:21:02 +0200

    Update defines.h

--- camera_prog/defines.h
+++ camera_prog/defines.h
@@ -3,12 +3,8 @@
 #include <string>
 using namespace std;
 
-namespace ARC
+namespace ARC_CAMERA
 {
-    const float TOP_SPEED=10;
-    
-    
-    const int COLS_TO_MEASURE=10;
     const double HORIZONTAL_FOV = 62.2;
     const double VERTICAL_FOV = 48.8;
     const int ILOWH = 32;
@@ -21,7 +17,6 @@ namespace ARC
     const double ANGEL_OF_CAMERA = 20;
     const int PIXEL_HEIGHT = 240;
     const int PIXEL_WIDTH = 320;
-    const int MESSURED_COL_RIGHT_LEFT_FROM_CENTER = 20;
     const int CANNY_THRE = 100;
     const int MAX_NUM_OBJECTS = 10;
     const int MIN_AREA = 100;
