5de4269e51d16480da1d2380cd690a4631a5ade2
antpo364 <antpo364@student.liu.se>
2017-05-15 10:30:46 +0200

    Update defines.h

--- camera_prog/defines.h
+++ camera_prog/defines.h
@@ -5,22 +5,22 @@ using namespace std;
 
 namespace ARC_CAMERA
 {
-    const double HORIZONTAL_FOV = 62.2;
-    const double VERTICAL_FOV = 48.8;
-    const int ILOWH = 32;
-    const int IHIGHH = 78;
-    const int ILOWS = 30; 
-    const int IHIGHS = 255;
-    const int ILOWV = 30;
-    const int IHIGHV = 255;
-    const double HEIGHT_OF_CAMERA = 0.32;
-    const double ANGEL_OF_CAMERA = 20;
-    const int PIXEL_HEIGHT = 240;
-    const int PIXEL_WIDTH = 320;
-    const int CANNY_THRE = 100;
-    const int MAX_NUM_OBJECTS = 10;
-    const int MIN_AREA = 100;
-    const int NUM_LAPS = 3;
+    const double HORIZONTAL_FOV = 62.2;     //Kamerans horisontella FOV
+    const double VERTICAL_FOV = 48.8;       //Kamerans vertikala FOV
+    const int ILOWH = 32;                   
+    const int IHIGHH = 78;                  
+    const int ILOWS = 30;                   //Bestämmer kontrastvillkoren för att vara ett hinder
+    const int IHIGHS = 255;                 
+    const int ILOWV = 30;                   
+    const int IHIGHV = 255;                 
+    const double HEIGHT_OF_CAMERA = 0.32;   //Kamerans höjd från marken
+    const double ANGEL_OF_CAMERA = 20;      //Vinkeln kameran är vriden
+    const int PIXEL_HEIGHT = 240;           //Bildskärmens upplösning  
+    const int PIXEL_WIDTH = 320;            
+    const int CANNY_THRE = 100;             //Sätter ribban för hur skarp kontrastskillnader måste vara
+    const int MAX_NUM_OBJECTS = 10;         //Maximala antalet objekt som inte antas vara brus
+    const int MIN_AREA = 100;               //Minsta arean för att kunna anses som ett objekt
+    const int NUM_LAPS = 3;                 //Antal varv bilen ska köra
 }
 
 #endif
